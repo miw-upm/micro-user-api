67e504eea278d1a0c6613a5243eb7cc5d3a2b4a4
JBernal <j.bernal@upm.es>
2025-03-07 15:43:34 +0100

    added Eureka

--- src/main/java/es/upm/miw/Application.java
+++ src/main/java/es/upm/miw/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
+
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
 public class Application {
     public static void main(String[] args) {
