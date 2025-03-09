9f0a8e05d7d37b1fde72b5114d26332749a21c66
JBernal <j.bernal@upm.es>
2025-03-09 19:02:25 +0100

    enhancement test

--- src/test/java/es/upm/miw/functionaltests/UserResourceFunctionalTest.java
+++ src/test/java/es/upm/miw/functionaltests/UserResourceFunctionalTest.java
@@ -2,50 +2,37 @@ package es.upm.miw.functionaltests;
 
 import es.upm.miw.data.models.User;
 import es.upm.miw.resources.UserResource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class UserResourceFunctionalTest {
-    @LocalServerPort
-    private int port;
     @Autowired
     private TestRestTemplate testRestTemplate;
-    private String baseUrl;
-
-    @BeforeEach
-    void setUp() {
-        this.baseUrl = "http://localhost:" + port + UserResource.USERS;
-    }
 
     @Test
     void testReadByIdentityOk() {
-        String url = this.baseUrl + "/pepe";
-        ResponseEntity<User> response = testRestTemplate.getForEntity(url, User.class);
+        ResponseEntity<User> response = testRestTemplate.getForEntity(
+                UserResource.USERS + UserResource.IDENTITY_ID, User.class, "pepe");
 
         assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-
-        User user = response.getBody();
-        assertThat(user).isNotNull();
-        assertThat(user.getIdentity()).isEqualTo("pepe");
+        assertThat(response.getBody())
+                .isNotNull()
+                .extracting(User::getIdentity).isEqualTo("pepe");
     }
 
     @Test
     void testReadByIdentityNotFound() {
-        String url = this.baseUrl + "/none";
         assertThat(this.testRestTemplate
-                .getForEntity(url, User.class)
+                .getForEntity(UserResource.IDENTITY_ID, User.class, "none")
                 .getStatusCode())
                 .isEqualTo(HttpStatus.NOT_FOUND);
     }
